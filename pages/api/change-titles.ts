45795ee861c37155526566e94773e8e69119dcf4
Vladyslav Nikitchuk <nikit4uk@gmail.com>
2023-03-18 21:06:53 +0200

    Update change-titles.ts

--- pages/api/change-titles.ts
+++ pages/api/change-titles.ts
@@ -18,33 +18,19 @@
 
 import { NextApiRequest, NextApiResponse } from 'next';
 import { connectToDatabase } from '@/lib/mongodb';
-
-interface UpdateResult {
-    matchedCount: number;
-    modifiedCount: number;
-    upsertedCount?: number;
-    upsertedId?: {
-        _id: string;
-    };
-}
+import { UpdateResult } from 'mongodb';
 
 export default async function handler(
     req: NextApiRequest,
-    res: NextApiResponse<UpdateResult>
+    res: NextApiResponse
 ) {
     const { collection, filter } = req.query;
     const body = JSON.parse(req.body);
 
-    if (typeof collection !== 'string' || typeof filter !== 'string') {
-        res.status(400).json({ message: 'Invalid request' });
-        return;
-    }
-
     const { db } = await connectToDatabase();
-    const result = await db.collection(collection).updateOne(
+    const result: UpdateResult = await db.collection(collection).updateOne(
         JSON.parse(filter),
         { $set: body }
     );
-
     res.status(200).json({ result });
 }
